ae38a594932804c18f7177fe1d4e4da1114d558e
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-02-12 11:24:45 -0300

    Refactor CLI interface

---
+++ Sniffo/Sniffo.CLI/CommandLineInterface.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using Sniffo.Core;
+
+namespace Sniffo.CLI
+{
+    public class CommandLineInterface
+    {
+        private IpHandler ipHandler;
+
+        public CommandLineInterface()
+        {
+            ipHandler = new IpHandler();
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Sniffio: A network packets Sniffer");
+            Console.WriteLine("Made By: TheLe0 - Leonardo Tosin");
+            Console.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+            ipHandler.InitSocket(SelectIPAddress());
+
+            if (ipHandler.Socket == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("IP: " + ipHandler.IPAddress);
+
+            if (ipHandler.Socket == null)
+            {
+                Console.WriteLine("Error: The socket was not initialized!");
+                return;
+            }
+
+            do
+            {
+                Console.WriteLine("## [ {0:yyyy-MM-dd HH:mm:ss.fff} ] ##".PadLeft(80, '#'), DateTime.Now);
+
+                Console.WriteLine(ipHandler.SniffedPackage());
+            } while (Console.KeyAvailable == false || Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        private IPAddress SelectIPAddress()
+        {
+            Console.WriteLine("Computador: " + ipHandler.Hostname);
+
+            var ipAddresses = ipHandler.GetAllAvaliableIp();
+
+            Console.WriteLine("Endereços IP:");
+            for (var i = 0; i < ipAddresses.Length; i++)
+            {
+                var ipAddress = ipAddresses[i];
+                Console.WriteLine("  " + (i + 1) + ") " + ipAddress);
+            }
+
+            int selectedIndex;
+            do
+            {
+                Console.Write("Selecione: ");
+            } while (int.TryParse(Console.ReadLine(), out selectedIndex) == false ||
+                     selectedIndex < 1 ||
+                     selectedIndex > ipAddresses.Length);
+
+            return ipAddresses[selectedIndex - 1];
+        }
+    }
+}
---
+++ Sniffo/Sniffo.Core/IpHandler.cs
@@ -0,0 +1,118 @@
+﻿using Sniffo.Domain;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Sniffo.Core
+{
+    public class IpHandler
+    {
+        public Socket? Socket { get; private set; }
+        public IPAddress? IPAddress { get; private set; }
+        public string Hostname { get; private set; }
+
+        public IpHandler()
+        {
+            Hostname = Dns.GetHostName();
+        }
+
+        public void InitSocket(IPAddress ipAddress)
+        {
+            IPAddress = ipAddress;
+
+            try
+            {
+                Socket = new Socket(
+                    IPAddress.AddressFamily,
+                    SocketType.Raw,
+                    ProtocolType.IP);
+
+                CreateSocket();
+            } 
+            catch (SocketException)
+            {
+                Console.WriteLine("Error: The socket could be not initialize. Verify your user permissions and if there's a firewall on the socket port");
+            }
+        }
+
+        public IPAddress[] GetAllAvaliableIp()
+        {
+            var hostnameEntry = Dns.GetHostEntry(Hostname);
+            var ipAddresses = hostnameEntry.AddressList;
+
+            return ipAddresses;
+        }
+
+        public string SniffedPackage()
+        {
+            return IpV4Package();
+        }
+
+        private string IpV4Package()
+        {
+            if (Socket == null)
+            {
+                return "";
+            }
+
+            var buffer = new byte[ushort.MaxValue];
+            var bufferLength = Socket.Receive(buffer);
+
+            var protocolIPv4 = new IPv4(buffer, bufferLength);
+
+            return protocolIPv4.ToString();
+        }
+
+        private void CreateSocket()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    CreateUnixSocket();
+                break;
+                case PlatformID.Win32NT:
+                    CreateWindowsSocket();
+                break;
+                default:
+                    throw new NotImplementedException("SocketType.Raw not mapped OS.");
+            }
+        }
+
+        private void CreateWindowsSocket()
+        {
+            if (IPAddress == null || Socket == null)
+            {
+                return;
+            }
+
+            var ipEndPoint = new IPEndPoint(IPAddress, 0);
+            Socket.Bind(ipEndPoint);
+
+            if (IPAddress.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                throw new NotImplementedException("SocketOptionName.HeaderIncluded works only for IPv4.");
+            }
+
+            Socket.SetSocketOption(
+                SocketOptionLevel.IP,
+                SocketOptionName.HeaderIncluded, // `HeaderIncluded` works only for IPv4.
+                true);
+
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                throw new NotImplementedException("IOControlCode.ReceiveAll works only on Windows.");
+            }
+
+            var optionIn = new byte[] { 1, 0, 0, 0 };
+            var optionOut = new byte[4];
+            Socket.IOControl(
+                IOControlCode.ReceiveAll, // `ReceiveAll` works only on Windows.
+                optionIn,
+                optionOut);
+        }
+
+        private void CreateUnixSocket()
+        {
+            throw new NotImplementedException("SocketType.Raw not works on Linux.");
+        }
+    }
+}
